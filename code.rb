c2c842cd102a21e41b29136ce25e90ed090226b8
MLampa <meidenbee@gmail.com>
2015-11-12 10:24:53 -0500

    add readme

--- code.rb
+++ code.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class SuperHero
   attr_reader :public_identity
   attr_accessor :species, :home, :fans_per_thousand, :powers, :weakness, :backstory
@@ -49,7 +50,7 @@ end
 
 class Speedster < SuperHero
   def backstory
-    "This is his backstory"
+    "This superhero grew up seeing the world extremely slowly. She later realized she had an amazing superpower of being really fast. She learned to control her power. At first, she used her power to rob banks and liquor stores; however, failing to stop a woman from getting shot and eventually killed turned her from a thief to a superhero, aiming to save lives instead of taking from them."
   end
 
   def powers
@@ -57,7 +58,7 @@ class Speedster < SuperHero
   end
 
   def weakness
-    "alcohol"
+    "consuming alcohol"
   end
 
   def speed_in_mph
@@ -67,7 +68,7 @@ end
 
 class Brawler < SuperHero
   def backstory
-    "The Brawler is an orphan from the Bronx. He took to working out to avenge the death of his parents when he was a child. He aims to rid the world of terrible people."
+    "This superhero started out as an orphan from the Bronx, NY. He took to working out to avenge the death of his parents when he was a child. He later gained superhuman strength. He aims to rid the world of terrible people."
   end
 
   def powers
@@ -85,7 +86,7 @@ end
 
 class Detective < SuperHero
   def backstory
-    "By day, the Detective works for the New York Police. By night, he takes his detective skills to track enemies."
+    "After his brother was murdered at just 12 years old, this superhero learned to track. He eventually found his brother's killer as none other than the city's kingpin, but realized the justice system was corrupt and that he was untouchable. He vowed to avenge his brother and others. By day, he works for the New York Police. By night, he takes his detective skills to track enemies and bring them to justice."
   end
 
   def powers
@@ -97,17 +98,17 @@ class Detective < SuperHero
   end
 
   def speed_in_mph
-    10
+    super / 6
   end
 end
 
 class Demigod < SuperHero
   def backstory
-    "This Demigod is from the distant solar system Avendia. As the only son of gods Hades and Persephone, he has the power to control lightning."
+    "This Demigod is from the distant solar system Avendia. As the only son of gods Hades and Persephone, he has many strengths but his main skill includes the ability to affect emotion. After seeing the horrible effects of his power, he refocused his energy to building his power over lightning."
   end
 
   def powers
-    "lightning"
+    "psychic"
   end
 
   def weakness
@@ -121,7 +122,7 @@ end
 
 class JackOfAllTrades < SuperHero
   def backstory
-    "The Jack of All Trades was sent to mimic those who impressed him as a child. He was burned terribly as a child and eventually learned to be others rather than himself."
+    "This superhero was sent to mimic those who impressed him as a child. He suffered a terrible acid burn on his face as an infant by a hateful ex-boyfriend of his adopted earth mother and grew up ridiculed everyday. He eventually learned to mimic others rather than be himself. His earth mother taught him to be kind. She was eventually killed in front of him by that same ex-boyfriend when this superhero was just a teenager and eventually learned to to use his powers to help the good people of this world against the rapists and murderers living among us."
   end
 
   def powers
@@ -147,7 +148,7 @@ end
 
 class WaterBased < SuperHero
   def backstory
-    "This water superhero almost drowned as a baby before a mermaid taught him to swim. He later was given the gift of amazing swimming skills."
+    "This water superhero almost drowned as a baby in the magical waters off the Gold Coast in Australia before a mermaid taught him to swim. He later was given the gift of amazing swimming skills."
   end
 
   def powers
